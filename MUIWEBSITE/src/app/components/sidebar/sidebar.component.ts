49a3a5cf56206094ed873fb550dc7a369ed32d36
kartikpawar10 <kartikpawarofficialon@gmail.com>
2024-09-21 17:47:55 +0530

    vfbfd

--- MUIWEBSITE/src/app/components/sidebar/sidebar.component.ts
+++ MUIWEBSITE/src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,9 +10,16 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SidebarComponent {
   filterForm!: FormGroup;
   showFiller: boolean = false;
+  mymap: Map<string, number> = new Map();
+  filteredData = new Set();
 
-  constructor(private fb: FormBuilder) {}
-
+  constructor(private fb: FormBuilder, private store: Store<any>) {
+    this.mymap.set('electronics', 1);
+    this.mymap.set('jewelery', 2);
+    this.mymap.set('mensClothing', 3);
+    this.mymap.set('womenClothing', 4);
+    this.filteredData;
+  }
   ngOnInit(): void {
     this.filterForm = this.fb.group({
       electronics: [false],
@@ -21,7 +29,17 @@ export class SidebarComponent {
     });
 
     this.filterForm.valueChanges.subscribe((selectedFilters) => {
-      console.log('Selected filters:', selectedFilters);
+      console.log(selectedFilters);
+      Object.keys(selectedFilters).forEach((key) => {
+        let a = Number(this.mymap.get(key));
+        if (selectedFilters[key]) {
+          console.log(1333, key);
+          this.filteredData.add(a);
+        } else if (!selectedFilters[key]) {
+          this.filteredData.delete(a);
+        }
+      });
+      console.log('Data', this.filteredData);
     });
   }
 }
